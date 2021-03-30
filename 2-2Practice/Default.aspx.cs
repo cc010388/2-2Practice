37452c818de6f22fb0d14f966ff12ae39b56ea7d
婕安 魏 <107111241@mail.oit.edu.tw>
2021-03-30 14:49:07 +0800

    pr2-2

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -8,7 +8,15 @@ using System.Web.UI.WebControls;
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            int n = 700;
+            if ((n % 4) ==0 || (n % 400) == 0 && (n % 100)! =0)
+            {
+                Response.Write("Y");
+            }
+            else
+            {
+                Response.Write("N");
+            }
         }
     }
 }
